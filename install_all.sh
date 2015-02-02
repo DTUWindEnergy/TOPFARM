781cd30addf8b6207a1cd1bbd3b6b87dd82aeb1a
Pierre-Elouan Réthoré <rethore@gmail.com>
2015-02-02 10:00:20 +0100

    update README

--- install_all.sh
+++ install_all.sh
@@ -1,10 +1,11 @@
-$!/bin/bash
+#!/bin/bash
 
-mkdir install_prerequisits
-cd install_prerequisits
+mkdir TOPFARM
+cd TOPFARM
 
 echo 'INSTALL Virtualenv'
-easy_install virtualenv
+sudo easy_install virtualenv
+
 virtualenv topfarmv
 . topfarmv/bin/activate
 pip install numpy scipy
@@ -17,7 +18,6 @@ go-openmdao-dev.py
 cd ..
 
 echo 'INSTALL FUSED-Wind'
-cd ..
 git clone https://github.com/FUSED-Wind/fusedwind.git
 cd fusedwind
 git checkout v0.1.0
@@ -25,13 +25,22 @@ plugin install
 cd ..
 
 echo 'INSTALL FUSED-Wake'
-git clone git@github.com:DTUWindEnergy/FUSED-Wake.git
-cd FUSED-Wake
-git checkout 0.1.0
-plugin install
-cd ..
+#git clone git@github.com:DTUWindEnergy/FUSED-Wake.git
+#cd FUSED-Wake
+#git checkout 0.1.0
+#plugin install
+#cd ..
+
+echo 'INSTALL other pre-requisits'
+pip install pandas matplotlib seaborn
 
 echo 'INSTALL TOPFARM'
 git clone git@github.com:DTUWindEnergy/TOPFARM.git
 cd TOPFARM
 plugin install
+cd ..
+
+
+echo 'To activate the virtual environment type:'
+echo '$ . activate'
+ln -s topfarmv/bin/activate .
