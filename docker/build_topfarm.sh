8189d070ecbc4731e41c92ff5b3f8bdeed83d2f6
Pierre-Elouan Réthoré <rethore@gmail.com>
2015-03-13 10:41:11 +0100

    Adding the Dockerfile

---
+++ docker/build_topfarm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# function: build topfarm
+# needs to be called with the command:
+# bash -c "source $OPENMDAODIR/bin/activate; build_topfarm.sh"
+
+cd $INSTALL_DIR
+git clone http://github.com/DTUWindEnergy/FUSED-Wake.git
+cd $INSTALL_DIR/FUSED-Wake/gclarsen
+python setup.py install
+cd $INSTALL_DIR/FUSED-Wake/py4we
+python setup.py install
+
+pip install -r $INSTALL_DIR/requirements.txt
