e89f3f94364f418fda2952f1ca260cc205b0b3dd
Pierre-Elouan Réthoré <rethore@gmail.com>
2015-03-24 17:11:06 +0100

    fixing the dockerfile

--- docker/build_topfarm.sh
+++ docker/build_topfarm.sh
@@ -16,14 +16,15 @@ omdao python setup.py install
 ### Installing Ipopt
 cd $INSTALL_DIR
 wget http://www.coin-or.org/download/source/Ipopt/Ipopt-3.12.1.tgz
-cd Ipopt
+tar xvf Ipopt-3.12.1.tgz
+cd Ipopt-3.12.1
 cd ThirdParty/Metis
-./get-Metis
+./get.Metis
 cd ..
 cd Mumps
-./get-Mumps
-cd ..
-./configure --prefix=/usr
+./get.Mumps
+cd ../..
+./configure --prefix=/usr/local
 make
 make install
 
@@ -31,5 +32,7 @@ make install
 # It also calls `pip install -r requirements.txt`
 omdao $INSTALL_DIR/fix_dependencies.sh
 
+touch /root/.bashrc
+touch /home/fusedwind/.bashrc
 echo 'export $LD_LIBRARY_PATH=/usr/local/lib'>>/home/fusedwind/.bashrc
 echo 'export $LD_LIBRARY_PATH=/usr/local/lib'>>/root/.bashrc
